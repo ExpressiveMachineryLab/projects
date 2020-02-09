b644516d23477f8a760c428a28da156caf25acf9
lynnecc98 <lynnecc98@gmail.com>
2020-02-08 19:33:31 -0500

    update

---
+++ IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/ToggleEmitters.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleEmitters : MonoBehaviour
+{
+    public GameObject SlowLeft;
+    public GameObject SlowRight;
+    public GameObject FastLeft;
+    public GameObject FastRight;
+
+    private bool slow = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TogglePanels()
+    {
+        if (slow)
+        {
+            SlowLeft.SetActive(false);
+            SlowRight.SetActive(false);
+            FastLeft.SetActive(true);
+            FastRight.SetActive(true);
+            slow = false;
+        }
+        else
+        {
+            SlowLeft.SetActive(true);
+            SlowRight.SetActive(true);
+            FastLeft.SetActive(false);
+            FastRight.SetActive(false);
+            slow = true;
+        }
+    }
+}
