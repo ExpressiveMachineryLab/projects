28d5e46a47807f56c5acf9a64428120e21eed7ee
Chloe Choi <lynnecc98@gmail.com>
2020-04-06 14:54:03 +0900

    Fixed layering issue + update to new panels

---
+++ IdeumPrototypes/XyloCraft/Ball Dropping/Assets/SelectedElement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectedElement : MonoBehaviour
+{
+    public Sprite Blue;
+    public Sprite Red;
+    public Sprite Green;
+    public Sprite Yellow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBlue() 
+    {
+        this.gameObject.GetComponent<Image>().sprite = Blue;
+    }
+
+    public void SetRed()
+    {
+        this.gameObject.GetComponent<Image>().sprite = Red;
+    }
+    public void SetGreen()
+    {
+        this.gameObject.GetComponent<Image>().sprite = Green;
+    }
+    public void SetYellow()
+    {
+        this.gameObject.GetComponent<Image>().sprite = Yellow;
+    }
+    public void SetWild()
+    {
+        Sprite[] imageArray = {Blue, Red, Green, Yellow};
+        this.gameObject.GetComponent<Image>().sprite = imageArray[(int)(Random.value * 10 / 4)];
+    }
+}
