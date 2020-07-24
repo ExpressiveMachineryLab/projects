7c74f798cf23a53356a9e8d431d515470b41b5d9
Michael Fewkes <mdfewkes@gmail.com>
2020-07-24 14:52:16 -0600

    Better outside click handling

--- IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/ClickOutsideToClose.cs
+++ IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/ClickOutsideToClose.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ClickOutsideToClose : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] alsoInclude;
+
 	void Update() {
 		if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)) {
 			this.gameObject.SetActive(ClickingSelfOrChild());
@@ -17,6 +21,16 @@ public class ClickOutsideToClose : MonoBehaviour {
 				return true;
 			};
 		}
+
+		foreach(GameObject parent in alsoInclude) {
+			RectTransform[] rectTransforms2 = parent.GetComponentsInChildren<RectTransform>();
+			foreach (RectTransform rectTransform in rectTransforms2) {
+				if (EventSystem.current.currentSelectedGameObject == rectTransform.gameObject) {
+					return true;
+				};
+			}
+		}
+
 		return false;
 	}
 }
