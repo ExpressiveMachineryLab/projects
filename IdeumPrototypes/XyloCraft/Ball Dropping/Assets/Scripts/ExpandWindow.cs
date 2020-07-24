da1453d70ecd4d8ae541ce86330155ec04bc0989
Michael Fewkes <mdfewkes@gmail.com>
2020-07-24 14:36:58 -0600

    Shapes menu closes when you click outside of it

--- IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/ExpandWindow.cs
+++ IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/ExpandWindow.cs
@@ -5,24 +5,10 @@ using UnityEngine;
 public class ExpandWindow : MonoBehaviour
 {
     public GameObject Window;
-    private bool expanded = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void Expand()
     {
         Debug.Log("expanded");
-        expanded = !expanded;
-        Window.SetActive(expanded);
+        Window.SetActive(!Window.activeSelf);
     }
 }
