a260c2d07aec6fb83a3ba300ae544d1c393c2fa6
Michael Fewkes <mdfewkes@gmail.com>
2020-07-24 01:40:55 -0600

    Added button functionality to SmallVideoPlayer

--- IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/SmallVideoPlayer.cs
+++ IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/SmallVideoPlayer.cs
@@ -6,25 +6,40 @@ using UnityEngine.Video;
 public class SmallVideoPlayer : MonoBehaviour
 {
 	public string videoFileName;
+	public bool playOnAwake = true;
 
 	private VideoPlayer videoPlayer;
+
+	private void Awake() {
+		if (videoPlayer != null) {
+			videoPlayer.time = 0;
+			videoPlayer.Play();
+		}
+	}
 
 	private void Start() {
 		videoPlayer = gameObject.GetComponent<VideoPlayer>();
 		videoPlayer.source = VideoSource.Url;
 		videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
 
+		if (playOnAwake) videoPlayer.Play();
+	}
+
+	public void PlayVideo() {
 		videoPlayer.Play();
 	}
 
-	private void Awake() {
-		if (videoPlayer != null) videoPlayer.Pause();
-
+	public void PauseVideo() {
+		videoPlayer.Pause();
 	}
 
-	private void OnMouseDown() {
+	public void StopVideo() {
+		videoPlayer.Pause();
+		videoPlayer.time = 0;
+	}
+
+	public void SkipVideo() {
+		StopVideo();
 		gameObject.SetActive(false);
 	}
-
-
 }
