ac8285496ed5922510d6f78b93ca5ab74b87da46
Michael Fewkes <mdfewkes@gmail.com>
2020-07-24 13:41:01 -0600

    More robust video player

--- IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/SmallVideoPlayer.cs
+++ IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/SmallVideoPlayer.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class SmallVideoPlayer : MonoBehaviour
 {
 	public string videoFileName;
 	public bool playOnAwake = true;
+	public string loadingText = "Loading...";
+	public string loadedText = "Ready...";
+	public GameObject[] relatedObjects;
 
 	private VideoPlayer videoPlayer;
+	private RawImage rawImage;
+	private Text text;
 
-	private void Awake() {
+	private void OnEnable() {
+
+		for (var i = 0; i < relatedObjects.Length; i++) {
+			relatedObjects[i].SetActive(true);
+		}
+
 		if (videoPlayer != null) {
 			videoPlayer.time = 0;
-			videoPlayer.Play();
+			if (playOnAwake) videoPlayer.Play();
 		}
 	}
 
@@ -22,24 +33,49 @@ public class SmallVideoPlayer : MonoBehaviour
 		videoPlayer.source = VideoSource.Url;
 		videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
 
-		if (playOnAwake) videoPlayer.Play();
+		text = gameObject.GetComponentInChildren<Text>();
+		if (!videoPlayer.isPrepared) {
+			text.text = loadingText;
+		}
+
+		rawImage = gameObject.GetComponent<RawImage>();
+
+		videoPlayer.Prepare();
+		if (playOnAwake) {
+			videoPlayer.Play();
+		}
+	}
+
+	private void Update() {
+		if (videoPlayer.isPrepared) {
+			text.text = loadedText;
+		}
+
+		if (videoPlayer.isPlaying) text.gameObject.SetActive(false);
 	}
 
 	public void PlayVideo() {
+		rawImage.texture = videoPlayer.targetTexture;
 		videoPlayer.Play();
 	}
 
 	public void PauseVideo() {
 		videoPlayer.Pause();
+		text.gameObject.SetActive(true);
 	}
 
 	public void StopVideo() {
+		rawImage.texture = null;
 		videoPlayer.Pause();
 		videoPlayer.time = 0;
+		text.gameObject.SetActive(true);
 	}
 
 	public void SkipVideo() {
 		StopVideo();
+		for (var i = 0; i <  relatedObjects.Length; i++) {
+			relatedObjects[i].SetActive(false);
+		}
 		gameObject.SetActive(false);
 	}
 }
