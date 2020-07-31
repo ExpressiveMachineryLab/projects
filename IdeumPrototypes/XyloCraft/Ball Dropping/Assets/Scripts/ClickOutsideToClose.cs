589109068d8d96aac1c82446cb9e07e9f03369d2
Michael Fewkes <mdfewkes@gmail.com>
2020-07-31 17:37:59 -0600

    Fixed click outside functionallity

--- IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/ClickOutsideToClose.cs
+++ IdeumPrototypes/XyloCraft/Ball Dropping/Assets/Scripts/ClickOutsideToClose.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ClickOutsideToClose : MonoBehaviour {
@@ -9,37 +10,37 @@ public class ClickOutsideToClose : MonoBehaviour {
 	[SerializeField]
 	private GameObject[] alsoInclude;
 
-	void LateUpdate() {
+	void Start()
+	{
+		Button button = gameObject.AddComponent<Button>();
+		button.interactable = true;
+		button.transition = Selectable.Transition.None;
+	}
+
+	void Update() {
 		if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)) {
-			this.gameObject.SetActive(ClickingSelfOrChild());
+			gameObject.SetActive(ClickingSelfOrChild());
 		}
 	}
 	private bool ClickingSelfOrChild() {
-		//Debug.Log("```Clicking Self Or Child");
-		//Debug.Log("```Current Selected Game Object: " + EventSystem.current.currentSelectedGameObject.name);
 		RectTransform[] rectTransforms = gameObject.GetComponentsInChildren<RectTransform>();
 		foreach (RectTransform rectTransform in rectTransforms) {
-			//Debug.Log("Checking " + rectTransform.gameObject.name);
-			if (EventSystem.current.currentSelectedGameObject == rectTransform.gameObject) {
-				//Debug.Log("```Found");
+			if (EventSystem.current.currentSelectedGameObject == rectTransform.gameObject)
+			{
 				return true;
 			}
 		}
 		
-		//Debug.Log("```Checking Included");
 		foreach (GameObject parent in alsoInclude) {
 			RectTransform[] rectTransforms2 = parent.GetComponentsInChildren<RectTransform>();
 			foreach (RectTransform rectTransform in rectTransforms2) {
-				//Debug.Log("Checking " + rectTransform.gameObject.name);
-				if (EventSystem.current.currentSelectedGameObject == rectTransform.gameObject) {
-					//Debug.Log("Found");
+				if (EventSystem.current.currentSelectedGameObject == rectTransform.gameObject)
+				{
 					return true;
 				}
 			}
 		}
 
-
-		//Debug.Log("```Not Found");
 		return false;
 	}
 }
